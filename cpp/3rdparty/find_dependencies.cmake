a83a7a72bacda88fc4e36ddba73b21e2388e6ef8
Meher Malladi <meher.malladi@uni-bonn.de>
2024-05-03 10:08:08 +0200

    add license to find_dep file

--- cpp/3rdparty/find_dependencies.cmake
+++ cpp/3rdparty/find_dependencies.cmake
@@ -1,4 +1,26 @@
-# TODO: Need to add kiss license since this is adapted from there
+# Modified from kiss-icp. following license is kiss-icp license
+# MIT License
+#
+# Copyright (c) 2022 Ignacio Vizzo, Tiziano Guadagnino, Benedikt Mersch, Cyrill
+# Stachniss.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 function(find_dep PACKAGE_NAME TARGET_NAME FETCH_CMAKE_FILE)
   if(NOT TARGET ${TARGET_NAME})
     message(STATUS "${PACKAGE_NAME}'s target: ${TARGET_NAME} not found.")
